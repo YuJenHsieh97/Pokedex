9b21b37f0223b2112d3f62e9a707321ce640fdfd
Celine Le <celine@Celines-MacBook-Pro.local>
2018-11-18 00:40:56 -0500

    Button for JS

--- main.js
+++ main.js
@@ -6,6 +6,16 @@ class Pokemon{
     this.attack = attack;
     this.defense = defense;
     this.ability = abilities;
+    this.container = document.getElementById('allpoke');
+    this.element = document.createElement('div');
+    this.element.className = 'product';
+  }
+
+  display(){
+    let pokeinfo = document.createElement("ol");
+    pokeinfo.innerHTML = [this.name,"HP: " + this.hp,"Attack: " + this.attack,"Defense: " + this.defense,"Abilities:" + this.ability];
+    this.element.appendChild(pokeinfo);
+    this.container.appendChild(this.element);
   }
 }
 //
@@ -13,45 +23,104 @@ class Pokemon{
 //   get(all)
 // }
 
-let info1 = document.getElementById('pokemons1');
-let pika ="http://fizal.me/pokeapi/api/v2/name/pikachu.json";
-axios.get(pika)
-  .then(function(response){
-    let abi = response.data.abilities;
-    let all1 = [];
-    for(let i=0;i<abi.length;i++){
-      all1.push(abi[i].ability.name);
-      info1.innerHTML=(all1);
-      pikAbi = info1;
+let pokeinfo;
+function pika(){
+  let pika ="http://fizal.me/pokeapi/api/v2/name/pikachu.json";
+  axios.get(pika)
+    .then(function(response){
+
+      let abi = response.data.abilities;
+      for(let i=0;i<abi.length;i++){
+        let all1 = [];
+        all1.push(abi[i].ability.name);
+      }
+
+      let name = "Pikachu";
+
+      let hp = response.data.stats[5].base_stat;
+
+      let attack = response.data.stats[4].base_stat;
+
+      let defense = response.data.stats[3].base_stat;
+
+      let pikk = new Pokemon(name,hp,attack,defense,abi);
+
+      document.getElementById('mb').style.display = "none";
+      document.getElementById('sb').style.display = "none";
+
+      pikk.display();
+
+   })
+}
+
+function mew(){
+  let mew ="http://fizal.me/pokeapi/api/v2/name/mewtwo.json";
+    axios.get(mew)
+
+    .then(function mew(response){
+      let abi2 = response.data.abilities;
+      for(let i=0;i<abi2.length;i++){
+        let all2 = [];
+        all2.push(abi2[i].ability.name);
+      }
+
+      let name = "Mewtwo";
+
+      let hp = response.data.stats[5].base_stat;
+
+      let attack = response.data.stats[4].base_stat;
+
+      let defense = response.data.stats[3].base_stat;
+
+      let mew = new Pokemon(name,hp,attack,defense,abi2);
+
+      document.getElementById('pb').style.display = "none";
+      document.getElementById('sb').style.display = "none";
+
+      mew.display();
+    })
+}
+
+function snor(){
+  let snorlax ="http://fizal.me/pokeapi/api/v2/name/snorlax.json";
+  axios.get(snorlax)
+
+    .then(function snor(response){
+      let abi3 = response.data.abilities;
+      for(let i=0;i<abi3.length;i++){
+        let all3 = [];
+        all3.push(abi3[i].ability.name);
+      }
+
+      let name ="Snorlax";
+
+      let hp = response.data.stats[5].base_stat;
+
+      let attack= response.data.stats[4].base_stat;
+
+      let defense= response.data.stats[3].base_stat;
+
+      let snor = new Pokemon(name,hp,attack,defense,abi3);
+
+      document.getElementById('mb').style.display = "none";
+      document.getElementById('pb').style.display = "none";
+
+      snor.display();
+    })
+}
+
+  function main(){
+      document.getElementById('pb').style.display = "initial";
+      document.getElementById('mb').style.display = "initial";
+      document.getElementById('sb').style.display = "initial";
+      snor.style.display = "none";
+      pikk.style.display = "none";
+      mew.style.display = "none";
     }
-  })
 
-
-// let pika = new Pokemon(pikaHP, name, )
-
-
-let info2 = document.getElementById('pokemons2');
-let mew ="http://fizal.me/pokeapi/api/v2/name/mewtwo.json"
-axios.get(mew)
-  .then(function mew(response){
-  let abi2 = response.data.abilities;
-  let all2 = [];
-  for(let i=0;i<abi2.length;i++){
-    all2.push(abi2[i].ability.name);
-    info2.innerHTML=(all2);
-    }
-  })
-
-
-
-let info3 = document.getElementById('pokemons3')
-let snorlax ="http://fizal.me/pokeapi/api/v2/name/snorlax.json"
-axios.get(snorlax)
-  .then(function snor(response){
-  let abi3 = response.data.abilities;
-  let all3 = [];
-  for(let i=0;i<abi3.length;i++){
-    all3.push(abi3[i].ability.name);
-    info3.innerHTML =(all3);
-    }
-  })
+    mb.addEventListener('click', mew);
+    mb.addEventListener("mouseout", main);
+    sb.addEventListener('click', snor);
+    sb.addEventListener("mouseout", main);
+    pb.addEventListener('click', pika);
+    pb.addEventListener("mouseout", main);
